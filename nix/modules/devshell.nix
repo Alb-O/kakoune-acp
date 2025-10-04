118baf693fae8eeaabf96a5e6ea6d785e5fdd200
Albert O'Shea <albertoshea2@gmail.com>
2025-10-04 10:23:42 +1000

    add kakoune to devshell

--- nix/modules/devshell.nix
+++ nix/modules/devshell.nix
@@ -1,18 +1,28 @@
 { inputs, ... }:
 {
-  perSystem = { config, self', pkgs, lib, ... }: {
-    devShells.default = pkgs.mkShell {
-      name = "kakount-acp-shell";
-      inputsFrom = [
-        self'.devShells.rust
-        config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
-      ];
-      packages = with pkgs; [
-        just
-        nixd # Nix language server
-        bacon
-        config.process-compose.cargo-doc-live.outputs.package
-      ];
+  perSystem =
+    { config
+    , self'
+    , pkgs
+    , lib
+    , ...
+    }:
+    {
+      devShells.default = pkgs.mkShell {
+        name = "kakount-acp-shell";
+        inputsFrom = [
+          self'.devShells.rust
+          config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
+        ];
+        packages = with pkgs; [
+          just
+          nixd # Nix language server
+          bacon
+          config.process-compose.cargo-doc-live.outputs.package
+
+          # For testing
+          kakoune
+        ];
+      };
     };
-  };
 }
